1672ba96ba2ea6e4f6949dd28648dd7917355f3b
dillon walsh <dillonwalsh93@gmail.com>
2021-06-13 13:06:34 -0400

    first question generated

--- index.js
+++ index.js
@@ -11,12 +11,28 @@ const questions = () => {
     return inquirer
         .prompt([
             {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of your project? (Required)',
+                validate: titleInput => {
+                    if (titleInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter a project title!');
+                        return false;
+                    }
 
-                // TODO: Create a function to write README file
-                function writeToFile(fileName, data) {}
+                }
+            },
+        ])
+}
+
+
+            // TODO: Create a function to write README file
+            // function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() { }
+// function init() { }
 
 // Function call to initialize app
-init();
+questions();
