d71d8f3049a499edf81a65973d1c6dbcc556b976
dillon walsh <dillonwalsh93@gmail.com>
2021-06-13 13:15:50 -0400

    added user inputs and license checkbox

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 // const questions = [];
+//begin readme title input
 const questions = () => {
     return inquirer
         .prompt([
@@ -24,15 +25,51 @@ const questions = () => {
 
                 }
             },
-        ])
-}
+            //         ])
+            // }
+            //end readme title input
+            //begin description input
+            {
+                type: "input",
+                name: "description",
+                message: "Use this section to describe your project:",
+
+            },
+
+            //end description input
+            //begin table of contents
+            //end table of contents
+            //begin usage input
+
+            {
+                type: "input",
+                name: "usage",
+                message: "What is the project's usage?",
+            },
+
+            //end usage input
+            //begin license
+            {
+                type: "checkbox",
+                name: "license",
+                message: "Choose the appropriate license for this project: ",
+                choices: ["ISC", "MIT", "Mozilla", "Open"],
+                validate: function (answer) {
+                    if (answer.length < 1) {
+                      return console.log("You must enter the license of your project.");
+                    }
+                    return true;
+                  },
+            },
+
+    
 
 
             // TODO: Create a function to write README file
             // function writeToFile(fileName, data) { }
 
-// TODO: Create a function to initialize app
-// function init() { }
+            // TODO: Create a function to initialize app
+            // function init() { }
 
-// Function call to initialize app
-questions();
+            // Function call to initialize app
+            questions();
