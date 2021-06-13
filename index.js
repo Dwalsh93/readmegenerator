12708423c7dfb16197ff5da926c84244dadd9909
dillon walsh <dillonwalsh93@gmail.com>
2021-06-13 12:23:26 -0400

    added inquirer

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// const { writeFile, copyFile } = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+const questions = () => {
+    return inquirer
+        .prompt([
+            {
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+                // TODO: Create a function to write README file
+                function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
